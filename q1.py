8b2cff72f71d15b6ed5fcf7e8a3074104ce96e03
Vinícius Teixeira <92376175+VinnieT1@users.noreply.github.com>
2024-03-17 13:08:17 -0300

    Finished q1

--- q1.py
+++ q1.py
@@ -33,8 +33,13 @@ b1 = [9, -2, 1]
 print('\na)')
 print('Sol. por Jacobi:', jacobi(A1, b1, 1e-10, 1000))
 print('Sol. por Gauss-Seidel:', gauss_seidel(A1, b1, 1e-10, 1000))
-print('Autovalores de A:', la.eigvals(A1))
-print('Raio espectral de A:', max(abs(la.eigvals(A1))))
+
+D1 = [[A1[i][j] if i == j else 0 for j in range(3)] for i in range(3)]
+R1 = [[0 if i == j else A1[i][j] for j in range(3)] for i in range(3)]
+T1 = la.inv(D1).dot(R1)
+
+print('Autovalores de T:', la.eigvals(T1))
+print('Raio espectral de T:', max(abs(la.eigvals(T1))))
 print()
 
 # b)
@@ -44,8 +49,13 @@ b2 = [18, -12, 17]
 print('b)')
 print('Sol. por Jacobi:', jacobi(A2, b2, 1e-10, 1000))
 print('Sol. por Gauss-Seidel:', gauss_seidel(A2, b2, 1e-10, 1000))
-print('Autovalores de A:', la.eigvals(A2))
-print('Raio espectral de A:', max(abs(la.eigvals(A2))))
+
+D2 = [[A2[i][j] if i == j else 0 for j in range(3)] for i in range(3)]
+R2 = [[0 if i == j else A2[i][j] for j in range(3)] for i in range(3)]
+T2 = la.inv(D2).dot(R2)
+
+print('Autovalores de T:', la.eigvals(T2))
+print('Raio espectral de T:', max(abs(la.eigvals(T2))))
 print()
 
 # c)
@@ -55,8 +65,13 @@ b3 = [-7, 9, 13]
 print('c)')
 print('Sol. por Jacobi:', jacobi(A3, b3, 1e-10, 1000))
 print('Sol. por Gauss-Seidel:', gauss_seidel(A3, b3, 1e-10, 1000))
-print('Autovalores de A:', la.eigvals(A3))
-print('Raio espectral de A:', max(abs(la.eigvals(A3))))
+
+D3 = [[A3[i][j] if i == j else 0 for j in range(3)] for i in range(3)]
+R3 = [[0 if i == j else A3[i][j] for j in range(3)] for i in range(3)]
+T3 = la.inv(D3).dot(R3)
+
+print('Autovalores de T:', la.eigvals(T3))
+print('Raio espectral de T:', max(abs(la.eigvals(T3))))
 print()
 
 # d)
@@ -75,6 +90,11 @@ b4 = [3, -6, -5, 0, 12, -12, -2, 2]
 print('d)')
 print('Sol. por Jacobi:', jacobi(A4, b4, 1e-10, 1000))
 print('Sol. por Gauss-Seidel:', gauss_seidel(A4, b4, 1e-10, 1000))
-print('Autovalores de A:', la.eigvals(A4))
-print('Raio espectral de A:', max(abs(la.eigvals(A4))))
+
+D4 = [[A4[i][j] if i == j else 0 for j in range(8)] for i in range(8)]
+R4 = [[0 if i == j else A4[i][j] for j in range(8)] for i in range(8)]
+T4 = la.inv(D4).dot(R4)
+
+print('Autovalores de T:', la.eigvals(T4))
+print('Raio espectral de T:', max(abs(la.eigvals(T4))))
 print()
